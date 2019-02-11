e7095f9fae0038996458f88692d929bfbbd0879a
parkchanjong <7000cj@gmail.com>
2019-02-11 20:30:15 +0900

    weather

--- parkjong/App.js
+++ parkjong/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+    error: null
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ isLoaded: true });
+        console.log(position);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
   render() {
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="dark-content" />
+        {isLoaded ? (
+          <Weather />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>날씨 준비중</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
       </View>
     );
   }
@@ -14,8 +41,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
   }
 });
